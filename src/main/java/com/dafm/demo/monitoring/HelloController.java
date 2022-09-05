d23cd6919b9e22b69f70669cd63e1d3e3ba12a98
Dawid <jedrzejczakdaw@gmail.com>
2022-09-05 14:43:05 +0200

    initial

---
+++ src/main/java/com/dafm/demo/monitoring/HelloController.java
@@ -0,0 +1,12 @@
+package com.dafm.demo.monitoring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
